661bf0e7123d5401a6e36fe031361391c5b8b894
Michelle Ling <michelleling225@gmail.com>
2017-09-16 15:13:39 -0700

    Finished hw

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a = a.uniq
+    a = a.map{|item| item.to_i + 2}
+    a = a.each_index.select{|i| a[i] < 10} 
+    a.reduce(0, :+)
   end
+
 end
 
 
